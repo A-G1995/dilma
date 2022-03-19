368e33ae607269238059f05c5c93ab7457e44d6c
Aryan <aryan@Aryans-MacBook-Pro.local>
2022-03-19 15:29:22 +0330

    Adds custom user model and manager

---
+++ core/managers.py
@@ -0,0 +1,46 @@
+import re
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+from .constants import IRANIAN_PHONE_NUMBER_REGEX_PATTERN
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, phone_number, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+
+        if not phone_number:
+            raise ValueError(_('The Phone Number must be set'))
+
+        phone_match = re.match(
+            IRANIAN_PHONE_NUMBER_REGEX_PATTERN, phone_number)
+        if phone_match is False:
+            raise ValueError(
+                _('The Phone Number is not a valid Iranian number'))
+
+        # email = self.normalize_email(extra_fields["email"])
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(phone_number, password, **extra_fields)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+from .constants import IRANIAN_PHONE_NUMBER_REGEX_PATTERN
 
 
 class Order(models.Model):
@@ -24,10 +28,33 @@ class Ticket(models.Model):
 
 
 # https://simpleisbetterthancomplex.com/tutorial/2018/01/18/how-to-implement-multiple-user-types-with-django.html
-class UserProfile(models.Model):
-    pass
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
 
+    phone_regex = RegexValidator(regex=IRANIAN_PHONE_NUMBER_REGEX_PATTERN,
+                                 message="Phone number must be entered in the format: '09121234567'. 11 digits allowed.")
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=11, unique=True)  # validators should be a list
 
-class User(models.AbstractUser):
-    profile = models.OneToOneField(
-        UserProfile, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(
+        _("first name"), max_length=150, null=False, blank=False)
+    last_name = models.CharField(
+        _("last name"), max_length=150, null=False, blank=False)
+    address = models.TextField(blank=True)
+
+    is_email_validated = models.BooleanField(default=False, null=False)
+    is_phone_number_validated = models.BooleanField(default=False, null=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']  # adding phone_number here cause an error in createsuperuser command
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = 'phone_number'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
